7c6d67de8f35932416bd406d010fcb23b981755d
Drinnn <pedro_lontro@hotmail.com>
2021-02-01 16:08:18 -0300

    test: add pixKey model test

---
+++ domain/model/pixKey_test.go
@@ -0,0 +1,34 @@
+package model_test
+
+import (
+	"testing"
+
+	"github.com/Drinnn/kool-pix/domain/model"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestModel_NewPixKey(t *testing.T) {
+	code := "001"
+	name := "Nubank"
+	bank, err := model.NewBank(code, name)
+
+	accountNumber := "abcnumber"
+	ownerName := "Pedro"
+	account, err := model.NewAccount(bank, accountNumber, ownerName)
+
+	kind := "email"
+	key := "p@p.com"
+	pixKey, err := model.NewPixKey(kind, account, key)
+
+	require.NotEmpty(t, uuid.FromStringOrNil(pixKey.ID))
+	require.Equal(t, pixKey.Kind, kind)
+	require.Equal(t, pixKey.Status, "active")
+
+	kind = "cpf"
+	_, err = model.NewPixKey(kind, account, key)
+	require.Nil(t, err)
+
+	_, err = model.NewPixKey("nome", account, key)
+	require.NotNil(t, err)
+}
