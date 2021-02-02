ee0eb78909a3e68ae1ebc08881ea1c185353a4b8
Drinnn <pedro_lontro@hotmail.com>
2021-02-02 14:23:16 -0300

    BREAKING CHANGE!: refactor models to deal with orm

--- domain/model/pixKey.go
+++ domain/model/pixKey.go
@@ -20,7 +20,7 @@ type PixKey struct {
 	Base      `valid:"required"`
 	Kind      string   `json:"kind" valid:"notnull"`
 	Key       string   `json:"key" valid:"notnull"`
-	AccountID string   `json:"account_id" valid:"notnull"`
+	AccountID string   `gorm:"column:account_id;type:uuid;not null" valid:"-"`
 	Account   *Account `valid:"-"`
 	Status    string   `json:"status" valid:"notnull"`
 }
@@ -45,11 +45,10 @@ func (pixKey *PixKey) isValid() error {
 func NewPixKey(kind string, account *Account, key string) (*PixKey, error) {
 
 	pixKey := PixKey{
-		Kind:      kind,
-		Key:       key,
-		Account:   account,
-		AccountID: account.ID,
-		Status:    "active",
+		Kind:    kind,
+		Key:     key,
+		Account: account,
+		Status:  "active",
 	}
 
 	pixKey.ID = uuid.NewV4().String()
